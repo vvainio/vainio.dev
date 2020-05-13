32feb19283b9980155cc52fbfebe169232570451
Ville Vainio <ville@vainio.dev>
2020-05-13 18:10:09 +0300

    Update years dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './index.scss'
+
+const qs = document.querySelector.bind(document)
+const currentYear = new Date().getFullYear()
+
+function updateCopyrightYear() {
+  const copyrightYear = qs('[data-id="copyright-year"]')
+  copyrightYear.innerHTML = currentYear
+}
+
+function updateExperienceYears() {
+  const experienceYears = qs('[data-id="experience-years"]')
+  const startingYear = 2014
+  experienceYears.innerHTML = currentYear - startingYear
+}
+
+updateCopyrightYear()
+updateExperienceYears()
